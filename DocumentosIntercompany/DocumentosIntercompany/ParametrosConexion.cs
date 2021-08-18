8503d0f8692185899628a17e424d4978f6815e2f
SAP Consultores <72764812+ConsultoresSAP@users.noreply.github.com>
2021-08-17 18:05:30 -0600

    Conexion a otra sociedad

---
+++ DocumentosIntercompany/DocumentosIntercompany/ParametrosConexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentosIntercompany
+{
+    class ParametrosConexion
+    {
+        public SAPbobsCOM.Company oCom { get; set; }
+        public string server { get; set; } 
+        public string TypeDB { get; set; }
+        public string Sociedad { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string SociedadName { get; set; }
+
+        
+    }
+}
